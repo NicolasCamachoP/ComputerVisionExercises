e107a74735223bc4064e521023a27b82f29240b7
Nicolás <nicolas-camacho@javeriana.edu.co>
2020-02-18 13:07:52 -0500

    Taller 3 Completo

--- Taller 3/Camacho_Florido_Transformaciones.cxx
+++ Taller 3/Camacho_Florido_Transformaciones.cxx
@@ -32,7 +32,7 @@ using namespace cv;
     return( -1);
   
   } // fi
-  imshow("Original", src);
+  //imshow("Original", src);
    //Create each transform matrix
    //Translation Matrix
     Mat Mt(2,3,CV_64F, cvScalar(0.0));
@@ -45,10 +45,10 @@ using namespace cv;
     
     //Rotation Matrix
     Mat Mr(2,3,CV_64F, cvScalar(0.0));
-    Mr.at<double>(0,0) = cos(35);    
-    Mr.at<double>(1,1) = cos(35);
-    Mr.at<double>(1,0) = sin(35); 
-    Mr.at<double>(0,1) = -sin(35);    
+    Mr.at<double>(0,0) = cos(0.610865);    //35 degrees = 0.610865 radians
+    Mr.at<double>(1,1) = cos(0.610865);
+    Mr.at<double>(1,0) = sin(0.610865); 
+    Mr.at<double>(0,1) = -sin(0.610865);    
     Mr.at<double>(1,2) = 1;
     Mr.at<double>(0,2) = 1;
 
@@ -62,50 +62,44 @@ using namespace cv;
     Ms.at<double>(0,2) = 0;
     
     //Matrix Multiplication
-    //Ms*Mr
-    Mat Msr(2,3,CV_64F, cvScalar(0.0));
-    Msr.at<double>(0,0) = 0.655322;    
-    Msr.at<double>(1,1) = 0.655322;
-    Msr.at<double>(1,0) = 0.458861; 
-    Msr.at<double>(0,1) = -0.458861;    
-    Msr.at<double>(1,2) = 0.8;
-    Msr.at<double>(0,2) = 0.8;
    
-    //Msr*Mt
+    //Ms.Mr.Mt
     Mat Msrt(2,3,CV_64F, cvScalar(0.0));
     Msrt.at<double>(0,0) = 0.655322;    
     Msrt.at<double>(1,1) = 0.655322;
     Msrt.at<double>(1,0) = 0.458861; 
     Msrt.at<double>(0,1) = -0.458861;    
     Msrt.at<double>(1,2) = -42.7191;
-    Msrt.at<double>(0,2) = -9.74567;
+    Msrt.at<double>(0,2) = -9.74565;
 
     //Write and show Test 1
     Mat resul1=Mat::zeros(src.rows,src.cols,src.type());
     warpAffine(src, resul1, Msrt, src.size());
-    imshow("A mano",resul1);
+    //imshow("Test 1",resul1);
     imwrite("Test1.png", resul1);
     
 
-    //Test 2, with functions
+    //Test 2, each operation separated
 
     
-    //Translation
+    //Moved
     Mat dstTrans;
     dstTrans=Mat::zeros(src.rows,src.cols,src.type());
     warpAffine(src,dstTrans,Mt,src.size());
-    imshow("Transladada",dstTrans);
+    //imshow("Transladada",dstTrans);
+    
+    //Rotation
+    Mat dstRot;
+    dstRot=Mat::zeros(src.rows,src.cols,src.type());
+    warpAffine(src,dstRot,Mr,src.size());
+    //imshow("Rotada",dstRot);
 
-    //Rotation and Scalling 
-    Mat dstRotated;
-    dstRotated=Mat::zeros(dstTrans.rows,dstTrans.cols,dstTrans.type());
-    Point center=Point(dstTrans.cols/2,dstTrans.rows/2);
-    double angle=-35;
-    Mat warp_dest_rot;
-    Mat rot_mat = getRotationMatrix2D( center, angle, 0.8);
-    warpAffine(dstTrans, dstRotated, rot_mat, dstTrans.size());
-    imshow("Rotada y escalada",dstRotated);
-    imwrite("Test2.png", dstRotated);
+    //Scalling
+    Mat dstScal;
+    dstScal=Mat::zeros(dstRot.rows,dstRot.cols,dstRot.type());
+    warpAffine(dstRot, dstScal, Ms, dstRot.size());
+    //imshow("Escalada",dstScal);
+    imwrite("Test2.png", dstScal);
     waitKey(0);
     return 0;
-  }
+  } 
